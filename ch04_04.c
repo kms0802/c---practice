853a14904137608868dbd2d70239a6b1cde5db00
kms0802 <kms0802@cju.ac.kr>
2025-03-22 16:57:34 +0900

    25.03.22

---
+++ ch04_04.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(void){
+    int a = 7, b =3;
+    float result;
+
+    result = (float)a / b;
+
+    printf("a = 7, b = 3, a / b =  %f\n", result);
+
+    return 0;
+}
